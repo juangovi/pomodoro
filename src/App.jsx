6b1414de618d0f1b7bb5ebf7afc57efefb547158
juangovi <juanantoniogovidal@gmail.com>
2022-09-22 19:33:18 +0200

    init

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import "./index.css"
+
+const App = () => {
+  let mins = 2;
+  let secs = 0;
+  const [time, setTime] = useState(
+    `${mins < 10 ? `0${mins}` : mins}:${secs < 10 ? `0${secs}` : secs}`
+  );
+  const [intervalId, setIntervalId] = useState(0);
+  const run = () => {
+    if (mins > 0 || secs > 0) {
+      if (secs == 0) {
+        secs = 59;
+        mins--;
+      } else {
+        secs--;
+      }
+      setTime(
+        `${mins < 10 ? `0${mins}` : mins}:${secs < 10 ? `0${secs}` : secs}`
+      );
+    }else{
+      clearInterval(intervalId)
+    }
+  };
+  const start = () => {
+    run()
+    setIntervalId(setInterval(run, 1000));
+  };
+  return (
+    <div className="container">
+      <div className="mx-auto text-center text-9xl">
+        {time}
+      </div>
+      <div className="text-xl flex justify-center">
+        <button onClick={start} className="px-5 py-2 rounded-full bg-blue-400  transition-all hover:bg-pink-500 duration-300">start</button>
+      </div>
+    </div>
+  );
+};
+
+export default App;
