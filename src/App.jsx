991aa78f5b028ddba0229e6e35d22185605596fd
juangovi <juanantoniogovidal@gmail.com>
2022-09-27 00:37:21 +0200

    estilos

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,11 @@ import Temp from './components/Temp'
 export const App = () => {
   return (
     <div className='min-h-screen flex flex-row divide-x bg-[#937DC2]'>
-      <div className='basis-1/4'>
+      <div className='basis-1/4 flex-row h-screen overflow-auto'>
         <button className='w-full py-3 text-center bg-[#C689C6]  transition-all hover:bg-[#FFABE1] duration-300'>add task</button>
-        <div className='w-full min-h-fit'>a</div>
+        <div className='w-full'>
+          
+        </div>
       </div>
       <div className='basis-3/4'>
         <Temp/>
